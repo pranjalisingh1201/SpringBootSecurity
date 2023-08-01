5d737ca0b1b9a8a32816be67a7dd1576b0f77463
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-08-01 12:42:29 +0530

    Spring Security

---
+++ src/main/java/com/springboot/secureapp/SpringBootSecure1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.secureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecure1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecure1Application.class, args);
+	}
+
+}
